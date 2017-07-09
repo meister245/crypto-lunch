c73bc54380121fa7770e756c81f22a5439e9927b
Zsolt Mester <22896196+meister245@users.noreply.github.com>
2017-07-09 12:12:18 +0200

    changes to cryptocompare api wrapper

--- modules/api/cryptocompare_api.py
+++ modules/api/cryptocompare_api.py
@@ -1,7 +1,7 @@
 import requests
 
 URL_COIN = 'https://www.cryptocompare.com/api/json/coinlist/'
-URL_PRICE = 'https://min-api.cryptocompare.com/json/'
+URL_PRICE = 'https://min-api.cryptocompare.com/data/'
 URL_SNAPSHOT = 'https://www.cryptocompare.com/api/data/'
 
 
@@ -10,28 +10,20 @@ class CryptoCompareAPI:
         pass
 
     def api_query(self, method, **params):
-
-        def fsym(method):
-            return 'fsym' if method == 'price' else 'fsyms'
+        url_params = []
 
         if method in ['coinlist']:
             return requests.get(URL_COIN)
 
         if method in ['coinsnapshot']:
-            url_params = []
-
             for k, v in params.items():
                 url_params.append(k + "=" + v)
 
-            return requests.get(URL_SNAPSHOT + method + '?' + "&".join(url_params))
+            return requests.get(URL_SNAPSHOT + method + '/?' + "&".join(url_params))
 
-        if method in ['price', 'pricemulti', 'pricemultifull']:
-            url_params = []
-
+        if method in ['pricemultifull']:
             for k, v in params.items():
-                if k in [fsym(method), 'tsyms']:
-                    url_params.append(k + "=" + ",".join(v))
-                else:
+                if k in ['fsyms', 'tsyms', 'e']:
                     url_params.append(k + "=" + v)
 
             return requests.get(URL_PRICE + method + '?' + "&".join(url_params))
@@ -45,7 +37,7 @@ class CryptoCompareAPI:
     def get_price_multi(self, params):
         return self.api_query('pricemulti', **params).json()
 
-    def get_price_multi_full(self, params):
+    def get_price_multifull(self, params):
         return self.api_query('pricemultifull', **params).json()
 
     def get_coin_snapshot(self, params):
